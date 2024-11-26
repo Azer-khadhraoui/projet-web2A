29c5a43e46e7b49d8962435abbe1b14d498e6e53
Azer-khadhraoui <azerronaldo2004@gmail.com>
2024-11-26 10:20:20 +0100

    Ajout de l'envoi d'email lors de la mise à jour du statut de l'utilisateur

--- view/frontoffice/update.php
+++ view/frontoffice/update.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
+
 include '../../Controller/usercontroller.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,14 +15,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $num = $_POST['num'];
     $pwd = $_POST['pwd'];
     $role = $_POST['role'];
-    $mail = $_POST['mail'];
+    $email = $_POST['mail']; 
     $statut = $_POST['statut'];
 
-    // Créez un utilisateur avec les nouveaux champs
-    $user = new User($cin, $fname, $lname, $pwd, $num, $role, $mail, $statut);
+   
+    $user = new User($cin, $fname, $lname, $pwd, $num, $role, $email, $statut);
 
     $controller = new UserController();
+    $oldUser = $controller->getUserByCin($cin);
     $controller->updateUser($user);
+
+    
+    if ($oldUser['statut'] == 0 && $statut == 1) {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'azerronaldo2004@gmail.com';
+            $mail->Password = 'pkjhmjhyuauhpqhl';
+            $mail->SMTPSecure = 'tls'; 
+            $mail->Port = 587;
+
+           
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+
+            $mail->setFrom('azerronaldo2004@gmail.com');
+            $mail->addAddress($email); 
+
+            $mail->isHTML(true);
+            $mail->Subject = "Account Status Changed";
+            $mail->Body = "Dear $fname $lname,<br><br>Your account status has been changed to blocked. Please contact the admin <br><br>Best regards,<br>GREEN & PURE";
+
+            $mail->send();
+
+            echo "<script>
+            alert('Email sent successfully');
+            window.location.href='affichage.php';
+          </script>";
+    exit();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
 
     header("Location: affichage.php");
     exit();
