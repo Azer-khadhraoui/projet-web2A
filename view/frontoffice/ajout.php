90d4f175f31f72321355eff9dd7070e3db659bd9
Azer-khadhraoui <azerronaldo2004@gmail.com>
2024-11-26 11:10:44 +0100

    Ajout de la vérification reCAPTCHA au formulaire sign up

--- view/frontoffice/ajout.php
+++ view/frontoffice/ajout.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['password'];
     $retype_password = $_POST['retype-password'];
     $mail = $_POST['mail'];
-    
+    $statut = 0; // Vous pouvez définir une valeur par défaut pour le statut
 
     // Vérifiez que les mots de passe correspondent
     if ($password !== $retype_password) {
@@ -17,11 +17,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    // Créez un nouvel utilisateur avec les nouveaux champs
-    $usr = new User($cin, $fname, $lname, $password, $number, 0, $mail,0);
+    // Vérifiez reCAPTCHA
+    $recaptcha_secret = '6LfReIoqAAAAAA8SADFIG8K-05njBLYPBU4Q7syB';
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
 
-    $controller = new usercontroller();
-    $controller->adduser($usr);
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    if (!$recaptcha->success) {
+        echo "La vérification reCAPTCHA a échoué. Veuillez réessayer.";
+        exit();
+    }
+
+    // Créez un nouvel utilisateur avec les nouveaux champs
+    $usr = new User($cin, $fname, $lname, $password, $number, 0, $mail, $statut);
+
+    $controller = new UserController();
+    $controller->addUser($usr);
 
     echo "Utilisateur ajouté avec succès.";
 } else {
